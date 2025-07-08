95faa640e110cbcf931387664ba4a7f39d42c7ad
yassine-youcefi <mohamed.youcefi.etu@univ-mosta.dz>
2025-07-08 12:04:59 +0400

    Add HashUtils class for password hashing and verification utilities

---
+++ app/auth/utils/hash_utils.py
@@ -0,0 +1,16 @@
+from bcrypt import checkpw, hashpw, gensalt
+from starlette.concurrency import run_in_threadpool
+
+class HashUtils:
+    @staticmethod
+    async def check_password(password: str, hashed_password: str) -> bool:
+        return await run_in_threadpool(
+            checkpw, password.encode("utf-8"), hashed_password.encode("utf-8")
+        )
+
+    @staticmethod
+    async def hash_password(password: str) -> str:
+        hashed = await run_in_threadpool(
+            hashpw, password.encode("utf-8"), gensalt()
+        )
+        return hashed.decode("utf-8")
